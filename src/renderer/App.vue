c182706dd3953360e01cc4a68a5c29dd52f0d0d6
Богдан Онищенко <dever4eg@gmail.com>
2019-03-31 23:50:32 +0300

    Add vuetify vue component

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+  <v-app>
+    <v-content transition="slide-x-transition">
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Vuetify from 'vuetify'
+import 'vuetify/src/stylus/main.styl'
 
+Vue.use(Vuetify)
 Vue.use(Router)
 
 export default new Router({
