2567aeea8ed473a51f878285b98a38acba8eb8e5
Богдан Онищенко <dever4eg@gmail.com>
2019-04-29 10:24:03 +0300

    Install material icons package

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,6 +5,9 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import 'vuetify/src/stylus/main.styl'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Vuetify from 'vuetify'
-import 'vuetify/src/stylus/main.styl'
 
 Vue.use(Vuetify)
 Vue.use(Router)
